b753631104f8d14d7c7810963560ab6bf935a198
pipoperezc <jorgeap72@yahoo.com.mx>
2020-11-04 17:07:29 -0700

    Conexion a base de datos

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,21 +28,22 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
             this.lArchivos = new System.Windows.Forms.ListBox();
             this.tDirectorio = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.tIp_Server = new System.Windows.Forms.TextBox();
+            this.tDatabase = new System.Windows.Forms.TextBox();
+            this.tUsuario = new System.Windows.Forms.TextBox();
+            this.tPassword = new System.Windows.Forms.TextBox();
+            this.tArchivo = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Image = global::Respaldo_Apsi.Properties.Resources.open;
-            this.button1.Location = new System.Drawing.Point(345, 14);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(33, 21);
-            this.button1.TabIndex = 0;
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // lArchivos
             // 
@@ -68,11 +69,124 @@
             this.label1.TabIndex = 3;
             this.label1.Text = "Directorio";
             // 
+            // tIp_Server
+            // 
+            this.tIp_Server.Location = new System.Drawing.Point(472, 40);
+            this.tIp_Server.Name = "tIp_Server";
+            this.tIp_Server.Size = new System.Drawing.Size(206, 20);
+            this.tIp_Server.TabIndex = 4;
+            // 
+            // tDatabase
+            // 
+            this.tDatabase.Location = new System.Drawing.Point(472, 89);
+            this.tDatabase.Name = "tDatabase";
+            this.tDatabase.Size = new System.Drawing.Size(206, 20);
+            this.tDatabase.TabIndex = 5;
+            // 
+            // tUsuario
+            // 
+            this.tUsuario.Location = new System.Drawing.Point(472, 128);
+            this.tUsuario.Name = "tUsuario";
+            this.tUsuario.Size = new System.Drawing.Size(206, 20);
+            this.tUsuario.TabIndex = 6;
+            // 
+            // tPassword
+            // 
+            this.tPassword.Location = new System.Drawing.Point(472, 174);
+            this.tPassword.Name = "tPassword";
+            this.tPassword.PasswordChar = '*';
+            this.tPassword.Size = new System.Drawing.Size(206, 20);
+            this.tPassword.TabIndex = 7;
+            // 
+            // tArchivo
+            // 
+            this.tArchivo.Location = new System.Drawing.Point(12, 219);
+            this.tArchivo.Name = "tArchivo";
+            this.tArchivo.Size = new System.Drawing.Size(325, 20);
+            this.tArchivo.TabIndex = 8;
+            // 
+            // button2
+            // 
+            this.button2.Image = global::Respaldo_Apsi.Properties.Resources.save;
+            this.button2.Location = new System.Drawing.Point(341, 219);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(30, 23);
+            this.button2.TabIndex = 9;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Image = global::Respaldo_Apsi.Properties.Resources.open;
+            this.button1.Location = new System.Drawing.Point(345, 14);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(33, 21);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(469, 21);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "IP del Servidor";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(469, 73);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(132, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Nombre de Base de Datos";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(471, 112);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Usuario";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(471, 160);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(61, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Contraseña";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(470, 217);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 14;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.tArchivo);
+            this.Controls.Add(this.tPassword);
+            this.Controls.Add(this.tUsuario);
+            this.Controls.Add(this.tDatabase);
+            this.Controls.Add(this.tIp_Server);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.tDirectorio);
             this.Controls.Add(this.lArchivos);
@@ -91,6 +205,17 @@
         private System.Windows.Forms.ListBox lArchivos;
         private System.Windows.Forms.TextBox tDirectorio;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tIp_Server;
+        private System.Windows.Forms.TextBox tDatabase;
+        private System.Windows.Forms.TextBox tUsuario;
+        private System.Windows.Forms.TextBox tPassword;
+        private System.Windows.Forms.TextBox tArchivo;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -20,7 +20,11 @@ namespace Respaldo_Apsi
         {
             InitializeComponent();
             CargaArchivos("c:\\copias");
-        }
+            tIp_Server.Text = "90.0.0.80";
+            tDatabase.Text = "APSISISTEMAS";
+            tUsuario.Text = "sa";
+            tPassword.Text = "apsi";
+         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
@@ -34,7 +38,7 @@ namespace Respaldo_Apsi
             if (oDialogo.ShowDialog() == DialogResult.OK)
             {
                 lcDirectorio = oDialogo.SelectedPath;
-                tDirectorio.Text = lcDirectorio;
+                
                 CargaArchivos(lcDirectorio);
             }
         }
@@ -43,19 +47,60 @@ namespace Respaldo_Apsi
         private void CargaArchivos(string lcDirectorio) 
         {// CargaArchivos
             if (Directory.Exists(lcDirectorio))
-            {// Directo
+            {// Directory
+                tDirectorio.Text = lcDirectorio;
                 DirectoryInfo odInfo = new DirectoryInfo(@lcDirectorio);
                 FileInfo[] files = odInfo.GetFiles("*.jpg");
                 foreach (FileInfo oArchivo in files)
                 {
                     lArchivos.Items.Add(oArchivo.Name);
                 }
-            }
+            } // Directory
             else 
             {
                 MessageBox.Show("No existe el Directorio" + lcDirectorio,"Error",MessageBoxButtons.OK,MessageBoxIcon.Error);
             }
 
         }// CargaArchivos
+
+        private void button2_Click(object sender, EventArgs e)
+        {//Click
+            string lcDirectorio = tDirectorio.Text.Trim();
+            SaveFileDialog oDialogoSave = new SaveFileDialog();
+            oDialogoSave.InitialDirectory = "@lcDirectorio";
+            oDialogoSave.Title = "Guardar Archivo Respaldo";
+            oDialogoSave.CheckFileExists = false;
+            oDialogoSave.CheckPathExists = true;
+            oDialogoSave.DefaultExt = "bak";
+            oDialogoSave.Filter = "Bakup Archivos (*.bak)|*.bak|All files (*.*)|*.*";
+            oDialogoSave.FilterIndex = 2;
+            oDialogoSave.RestoreDirectory = true;
+            if (oDialogoSave.ShowDialog() == DialogResult.OK)
+            {
+
+                tArchivo.Text = oDialogoSave.FileName;
+
+            }
+        }//Click
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            string lcIP = tIp_Server.Text.Trim();
+            string lcDatabase = tDatabase.Text.Trim();
+            string lcUser = tUsuario.Text.Trim();
+            string lcPass = tPassword.Text.Trim();
+            string lcRuta = tArchivo.Text.ToLower();
+            string lcQuery = "Backup database " + lcDatabase + " to disk = '" + lcRuta + "'";
+            
+            string lcConString = "Data Source=" + lcIP + ";Database="+lcDatabase+";data source=.; uid="+lcUser+"; pwd="+lcPass+";";
+            SqlConnection oConexion = new SqlConnection(lcConString);
+            oConexion.Open();
+            SqlCommand oComando = new SqlCommand(lcQuery,oConexion);
+            oComando.ExecuteNonQuery();
+
+
+
+        }
     }
 }
